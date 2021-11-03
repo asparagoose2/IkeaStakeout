48d9bc94c9ea06399a8d583e1f0322dac4d81e56
Ofir <ofir.duchovne@gmail.com>
2021-11-03 20:19:57 +0200

    change db structure

--- plugins/memory/in_memory.py
+++ plugins/memory/in_memory.py
@@ -3,18 +3,24 @@ from .base import AbstractMemoryPlugin
 
 class InMemoryPlugin(AbstractMemoryPlugin):
     def __init__(self):
-        self._data: dict[str, set] = dict()
+        self._data: dict[str, dict[str,set]] = dict()
 
     async def get_all_products(self) -> list:
-        return list(self._data.keys()).copy()
+        return self._data.copy()
 
-    async def get_chat_ids_for_products(self, product_id: str) -> set:
-        return self._data.get(product_id, set()).copy()
+    async def product_has_no_subscribers(self, product_id: str):
+        for key in self._data[product_id]:
+            if not self._data[product_id][key] == set():
+                return False
+        return True
 
-    async def subscribe(self, product_id: str, chat_id: int) -> None:
-        self._data.setdefault(product_id, set()).add(chat_id)
+    async def get_chat_ids_for_products(self, product_id: str, store_id: str) -> list:
+        return self._data.get(product_id, dict()).get(store_id, list()).copy()
 
-    async def unsubscribe(self, product_id: str, chat_id: int) -> None:
-        self._data[product_id].discard(chat_id)
-        if product_id in self._data and not self._data[product_id]:
+    async def subscribe(self, product_id: str, store_id: str, chat_id: int):
+        self._data.setdefault(product_id, dict()).setdefault(store_id,set()).add(chat_id)
+
+    async def unsubscribe(self, product_id: str, store_id: str, chat_id: int):
+        self._data[product_id][store_id].discard(chat_id)
+        if product_id in self._data and self.product_has_no_subscribers(product_id):
             self._data.pop(product_id)
